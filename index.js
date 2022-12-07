54f8948617f6bad9f5cf071f6602733600cfe5f4
ianK-code <kian99564@gmail.com>
2022-12-07 09:07:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+
+function reverse(word){
+  // mom => mom
+  // racecar => racecar
+  const wordArr = word.split("")
+  const reverseWordArr = wordArr.reverse()
+  const reversedWord = reverseWordArr.join("")
+  
+  return reversedWord
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  //reverse the input string
+  const reverseWord = reverse(word)
+
+  // If the input is the same as the reversed input
+  if (word === reverseWord){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
+
+  if the reversed string is the same as the input
+    return true
+  else
+    return false*/
+
+
+/*
+reverse the input string
 
 /*
   Add written explanation of your solution here
